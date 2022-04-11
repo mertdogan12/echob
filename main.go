d1eaef894a772eedbe461a178ed988ca24be266d
Mert Dogan <mert.dogan@me.com>
2022-04-12 01:57:01 +0200

    Clear the terminal beetween inputs
    .

--- main.go
+++ main.go
@@ -9,10 +9,19 @@ import (
 
 func main() {
 	br := bufio.NewReader(os.Stdin)
+	input := make([]byte, 0)
+
 	for {
 		line, _, _ := br.ReadLine()
 		if len(line) != 0 {
-			fmt.Println(hex.Dump(line))
+			input = append(input, []byte(line)...)
+
+			clear()
+			fmt.Println(hex.Dump(input))
 		}
 	}
+}
+
+func clear() {
+	fmt.Print("\033[H\033[2J")
 }
